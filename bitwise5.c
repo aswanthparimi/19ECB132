25368799d5899649909ead2b0d96ba15bbbb5d4a
aswanthparimi <101494078+aswanthparimi@users.noreply.github.com>
2022-05-31 12:24:17 +0000

    added output

--- bitwise5.c
+++ bitwise5.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-main()
+int  main()
 {int a,b;
 printf("enter two integers");
 scanf("%d%d",&a,&b);
@@ -7,3 +7,10 @@ printf("a&b=%d",a&b);
 printf("\n a|b=%d",a|b);
 printf("\n a^b=%d",a^b);
 }
+
+
+
+enter two integers4 5
+a&b=4
+ a|b=5
+ a^b=1
