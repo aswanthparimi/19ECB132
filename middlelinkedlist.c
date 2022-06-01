71b5d9b78f8cccce340c8362b4f3389f3303bbe0
aswanthparimi <101494078+aswanthparimi@users.noreply.github.com>
2022-06-01 04:43:20 +0000

    added output

--- middlelinkedlist.c
+++ middlelinkedlist.c
@@ -71,7 +71,7 @@ int main()
 }
 
 
-
+*/output
 5->NULL
 The middle element is [5]
 
@@ -85,4 +85,4 @@ The middle element is [4]
 The middle element is [4]
 
 1->2->3->4->5->NULL
-The middle element is [3]
+The middle element is [3]*/
